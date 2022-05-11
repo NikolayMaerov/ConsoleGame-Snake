ceac1fbad37931cd99f158915e1d24301bb2bd62
NikolayMaerov <74756664+NikolayMaerov@users.noreply.github.com>
2022-05-11 21:32:15 +0400

    Console snake game

---
+++ ConsoleGame-Snake/VertikalLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VertikalLine : Figure
+    {
+        public VertikalLine(int yUp, int uDown, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yUp; y <= uDown; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+    }
+}
